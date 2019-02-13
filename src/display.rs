e9d58e5bbba86fb02f44c7b421f75f444f4e02fe
Alex Ragone <alex.ragone@ezyvet.com>
2019-02-13 21:06:08 +1300

    feat: improved output

--- src/display.rs
+++ src/display.rs
@@ -10,30 +10,48 @@ pub fn render(lint_messages: Vec<LintMessage>) {
     }
     // Group the ouput by file
     for (file, outputs) in &lint_messages.into_iter().group_by(|elt| elt.file.to_owned()) {
-        let project_root = get_project_root(file);
-
-        for lint_message in outputs.sorted_by_key(|line_message| line_message.line) {
-            print_lint_message(lint_message, &project_root);
-        }
+        let project_root = get_project_root(&file);
+        let file_name = file.strip_prefix(&project_root).unwrap().to_str().unwrap();
+        outputs
+            .group_by(|lint_message| lint_message.line)
+            .into_iter()
+            .for_each(|(line, lint_messages)| {
+                println!("{}:{}", file_name.green(), line.to_string().dimmed());
+                print_lint_message(lint_messages.collect(), line);
+            });
     }
 }
 
 /// Print the lint message to stdout
-fn print_lint_message(lint_message: LintMessage, project_root: &PathBuf) {
-    let file_name = lint_message.file.strip_prefix(&project_root).unwrap().to_str().unwrap();
-    let line_number = lint_message.line.to_string();
-    let source = lint_message.source;
-    let message = lint_message.message;
-    let linter = lint_message.linter;
-    println!("{}:{}", file_name.green().bold(), line_number.dimmed());
-    println!("{}{}{} {}", "[", linter.blue(), "]", message);
-    println!("{}", source.dimmed());
+fn print_lint_message(lint_messages: Vec<LintMessage>, line: u32) {
+    let line_string = line.to_string();
+    let padding = &str::repeat(" ", line_string.len());
+    let source = &lint_messages.first().unwrap().source;
+    let vertical_line = format!("{} {}", padding, "|".blue());
+    println!("{}", vertical_line);
+    println!("{} {} {}", line_string.blue(), "|".blue(), source);
+    println!("{}", vertical_line);
+
+    lint_messages
+        .into_iter()
+        .group_by(|lint_message| lint_message.linter.to_owned())
+        .into_iter()
+        .for_each(|(linter, lint_messages)| {
+            println!("[{}]",linter.blue().bold());
+            lint_messages
+                .into_iter()
+                .for_each(|lint_message| {
+                    let message = lint_message.message;
+                    let linter = lint_message.linter;
+                    println!("{} {}", "-->".blue(), message.bold());
+                });
+        });
     println!("");
 }
 
 
 /// Recrsively looks for a parent directory containing .git and returns the path
-fn get_project_root(file: PathBuf) -> PathBuf {
+fn get_project_root(file: &PathBuf) -> PathBuf {
     for entry in WalkDir::new(&file)
         .into_iter()
         .filter_map(|e| e.ok()) {
@@ -43,5 +61,5 @@ fn get_project_root(file: PathBuf) -> PathBuf {
             }
         }
     let parent_file = file.parent().to_owned().unwrap().to_path_buf();
-    get_project_root(parent_file)
+    get_project_root(&parent_file)
 }
