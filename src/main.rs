4fafac486e0d5566c954455d3d62dad2d8b2eb93
Alex Ragone <alex.ragone@ezyvet.com>
2019-02-18 22:42:49 +1300

    updated docs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-//! This tool aims to run multiple linters on a commit range compatible with `git`.
+//! This tool aims to run **multiple** linters on a commit range compatible with `git`.
 //!
-//! Linters are great tools to enforce code style in your code, but it has some limitations: it can only lint entire files.
-//! When working with legacy code, we often have to make changes to very large files (which would be too troublesome to fix all lint errors)
-//! and thus it would be good to lint only the lines changed and not the entire file.
+//! Inspired by [lint-diff](https://github.com/grvcoelho/lint-diff) and [lint-staged](https://github.com/okonet/lint-staged)
+//! > Linters are great tools to enforce code style in your code, but it has some limitations: it can only lint entire files.
+//! > When working with legacy code, we often have to make changes to very large files (which would be too troublesome to fix all lint errors)
+//! > and thus it would be good to lint only the lines changed and not the entire file.
 //!
-//! `lint-emit` receives a commit range and uses the specified linters to lint the changed files and filter only the errors introduced in the commit range (and nothing more).
+//! > `lint-emit` receives a commit range and uses the specified linters to lint the changed files and filter only the errors introduced in the commit range (and nothing more).
 //!
 //! # Configuration
 //! You can add a linter by editing the config file found in your xdg path.
